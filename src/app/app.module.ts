00f96516cee836c78f7b483f836cf001f8497cb3
Daniel Francisco Rojas <2022160045udh.edu.pe>
2025-09-07 12:09:57 -0500

    feat: implement login functionality with PrimeNG components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { StudentsComponent } from './students/students.component';
+
+// Primeng
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,13 @@ import { StudentsComponent } from './students/students.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    PanelModule,
+    InputTextModule,
+    ToggleButtonModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth';
 
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  providers: [AuthService]
 })
 export class LoginComponent {
+  constructor(private auth: AuthService) { }
+  usuario: string;
+  clave: string;
 
+  ngOnInit() {
+  }
+
+  ingresar() {
+    this.auth.login(this.usuario, this.clave);
+  }
 }
